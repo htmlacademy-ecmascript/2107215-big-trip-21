ec5d9a0c51fadba10407f999493621c2bdf4664e
Alena59 <asdalena59@gmail.com>
2023-08-20 12:35:13 +0300

    правки

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -104,7 +104,7 @@ const createPointEditTemplate = ({ point = POINT_EMPTY, pointDestination, pointO
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
           <div class="event__available-offers">
-          ${createOfferSelectorTemplate(offersByType, point)}
+          ${createOfferSelectorTemplate(offersByType)}
           </div>
         </section>
         <section class="event__section  event__section--destination">
